84994c4cde852f1917816115d2194f4a6b4b0663
Gustavo Lopes <38574162+gustavolopesv3@users.noreply.github.com>
2021-05-07 16:55:45 -0500

    deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,24 +2,26 @@ import Head from "next/head";
 import { useEffect, useState } from "react";
 import { api } from "../services/api";
 import styles from "../styles/Home.module.css";
-import { Button, Table, Container} from "react-bootstrap";
+import { Button, Table, Container } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Link from "next/link";
-
-
 
 export default function Home() {
   const [phones, setPhones] = useState([]);
 
   useEffect(() => {
-    api.get("/").then((res) => setPhones(res.data));
+    getContact();
   }, []);
+
+  const getContact = async () => {
+    await api.get(`/`).then((res) => setPhones(res.data));
+  };
 
   return (
     <>
       <Container>
         <h1> PROJETO TECNOLOGIAS WEB</h1>
-        <Table striped bordered hover  variant="dark">
+        <Table striped bordered hover variant="dark">
           <thead>
             <tr>
               <th>Nome</th>
@@ -44,20 +46,12 @@ export default function Home() {
           </tbody>
         </Table>
 
-
         <Container className="alinhar-botoes">
           <Link href="/contact">
             <Button variant="warning">Criar novo contato</Button>
-          </Link>      
+          </Link>
         </Container>
-
-     </Container>
-
-
-
+      </Container>
     </>
-
-
   );
-  
 }
